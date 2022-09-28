97889519486422899ff76fa16d0ee17d6660d1e1
ashley <ashley.oketo@student.moringaschool.com>
2022-09-28 11:48:23 +0300

    Completed

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,17 +2,24 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return (<div> 
-    <h1>Movies Page</h1>
 
-    {movies.map((movie)=>(
-    <div key={movie.title}>
-      {movie.title}
-      {movie.time}
-      </div>
-    )
-    )}
-</div>
-)}
+  const movie = movies.map((movie)=>(
+    <div>
+      <h2>{movie.title}</h2>
+      <h3>{movie.time}</h3>
+      <ul>
+        {movie.genres.map((genre)=>(
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  ))
+  return(
+    <div>
+      <h1>Movies Page</h1>
+      <div>{movie}</div>
+    </div>
+  ) 
+}
 
 export default Movies;
